2193eccaefc0999ad5b6a7971bd7be58841e4229
Motasem94 <amotasem.ahmed@gmail.com>
2021-01-11 23:54:43 +0200

    1st edit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello");
+})
+
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server started @ port 3000");
+})
