cd428409afb4b28d2c977ef962a5cec4f11b8dbf
Motasem94 <amotasem.ahmed@gmail.com>
2021-01-12 01:21:34 +0200

    Add ejs

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     var today = new Date();
-    var currentDay = today;
-    if (currentDay === 6 || currentDay === 0) {
-        res.sendFile(__dirname+"/weekend.html");
-    } else {
-        res.sendFile(__dirname+"/weekday.html");
-    }
+    var currentDay = today.getDay();
+    var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    res.render("list",{kindOfDay: weekday[currentDay]});
 })
 
 
