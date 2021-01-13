b8955b3a34591cad746c7a48477b149891c611bd
Motasem94 <amotasem.ahmed@gmail.com>
2021-01-13 08:46:04 +0200

    Showing only Date

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     var today = new Date();
-    var currentDay = today.getDay();
-    var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    res.render("list",{kindOfDay: weekday[currentDay]});
+    var options ={
+        weekday:'long',
+        day:'numeric',
+        month:'long'
+    };
+    var day = today.toLocaleDateString("en-US",options);
+    res.render("list",{kindOfDay: day});
 })
 
 
