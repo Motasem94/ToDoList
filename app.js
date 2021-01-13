75e13e47cba33e8059ddfbd6ec5275da35ca9dd5
Motasem94 <amotasem.ahmed@gmail.com>
2021-01-13 22:52:38 +0200

    fix routes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     let item = req.body.newItem;
-    if (req.body.list === "Work list") {
+    if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
     } else {
