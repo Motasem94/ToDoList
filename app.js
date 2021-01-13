46d3a6ae8fa7123618818c8c1f2b5bc56ecec316
Motasem94 <amotasem.ahmed@gmail.com>
2021-01-13 22:44:19 +0200

    routes logic

--- app.js
+++ app.js
@@ -6,28 +6,41 @@ let items = [];
 let workItems = [];
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     let today = new Date();
-    let options ={
-        weekday:'long',
-        day:'numeric',
-        month:'long'
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
     };
-    let day = today.toLocaleDateString("en-US",options);
-    res.render("list",{listTitle: day, newListItems: items});
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
+    });
 })
 
-app.post("/",(req,res)=>{
-    item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    if (req.body.list === "Work list") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 })
 
-app.get("/work",(req,res)=>{
-    res.render("list",{listTitle: "Work", newListItems: workItems})
+app.get("/work", (req, res) => {
+    res.render("list", {
+        listTitle: "Work",
+        newListItems: workItems
+    })
 })
 
 
