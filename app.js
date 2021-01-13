c3e177a105066acc8d6929f5a2841132f5a92fd5
Motasem94 <amotasem.ahmed@gmail.com>
2021-01-13 21:16:15 +0200

    add style

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+let items = [];
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    var today = new Date();
-    var options ={
+    let today = new Date();
+    let options ={
         weekday:'long',
         day:'numeric',
         month:'long'
     };
-    var day = today.toLocaleDateString("en-US",options);
+    let day = today.toLocaleDateString("en-US",options);
     res.render("list",{kindOfDay: day, newListItems: items});
 })
 
