cd5d312afefa8dc52e28a5ac9c4f6fab289ea74f
Motasem94 <amotasem.ahmed@gmail.com>
2021-02-19 08:22:54 +0200

    added ProcFile

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-
 const app = express();
 
 app.set("view engine", "ejs");
@@ -145,6 +144,8 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
